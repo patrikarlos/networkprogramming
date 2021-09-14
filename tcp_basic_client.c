a83101d1999f8c35386980e9afdc2b1b0152ecf0
patrik arlos <patrik.arlos@bth.se>
2021-09-14 14:50:25 +0200

    Added various error checkings.

--- tcp_basic_client.c
+++ tcp_basic_client.c
@@ -11,21 +11,22 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-
 #include <arpa/inet.h>
 
 #define PORT "4950" // the port client will be connecting to 
-
 #define MAXDATASIZE 1401 // max number of bytes we can get at once 
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+	if (sa->sa_family == AF_INET6) {
+	  	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+	}
+	printf("Unknown FAMILY!!!!\n");
+	return(0); 
 }
 
 int main(int argc, char *argv[])
@@ -66,26 +67,33 @@ int main(int argc, char *argv[])
             perror("client: connect");
             continue;
         }
-
+	printf("Connection successfull.\n");
         break;
     }
-
+    /* Why are we here??? p == NULL, Looped no match (socket &| connect).. 
+       Success?? */
+    
     if (p == NULL) {
-        fprintf(stderr, "client: failed to connect\n");
+        fprintf(stderr, "client: failed to socket||connect\n");
+	/* Clear servinfo */
+	freeaddrinfo(servinfo); // all done with this structure
         return 2;
     }
 
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),s, sizeof s);
-    printf("client: connecting to %s\n", s);
+    int q=inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),s, sizeof s);
+    /* q ok or not */
+    printf("client: connected to %s:%s\n", s,PORT);
 
     freeaddrinfo(servinfo); // all done with this structure
     int totalBytes=0;
-	numbytes=send(sockfd,trunk,sizeof(trunk),0);
-	printf("Sent %d bytes to server\n",numbytes);
+    
+    numbytes=send(sockfd,trunk,sizeof(trunk),0);
+    printf("Sent %d bytes to server\n",numbytes);
     while(1){
       numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0);
       if(numbytes == -1 ){
         perror("recv");
+	/* close socket */
         exit(1);
       }
       if (numbytes == 0) {
@@ -101,6 +109,8 @@ int main(int argc, char *argv[])
       }
       
       printf("client (%d/%d) : received '%s'\n",numbytes, totalBytes, buf);
+      /*      sleep(2); */
+      numbytes=send(sockfd,trunk,sizeof(trunk),0);
     }
     close(sockfd);
 
